22def39a0eedc77ec11f9e7027ba9341a889ee1d
LaperCode <lapper2005@gmail.com>
2024-11-04 17:42:06 +0700

    Doing HoaDon __Not finished yet

--- QuanLySieuThiMini/src/QuanLySieuThiMiNi/DanhSachHoaDon.java
+++ QuanLySieuThiMini/src/QuanLySieuThiMiNi/DanhSachHoaDon.java
@@ -1,4 +1,5 @@
 package QuanLySieuThiMiNi;
 
 public class DanhSachHoaDon {
+
 }
--- QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
+++ QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
@@ -2,7 +2,7 @@ package QuanLySieuThiMiNi;
 
 public class QuanLySieuThiMini {
     public static void main(String[] args) {
-
+        System.out.println("Hello team");
 
     }
 }
