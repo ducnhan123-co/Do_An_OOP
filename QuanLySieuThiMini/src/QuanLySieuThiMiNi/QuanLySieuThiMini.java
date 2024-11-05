9416c054cad6ed8a3a8086adacd22970cafb1876
LaperCode <lapper2005@gmail.com>
2024-11-05 16:57:34 +0700

    chua xong hoa don files

--- QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
+++ QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
@@ -2,11 +2,7 @@ package QuanLySieuThiMiNi;
 
 public class QuanLySieuThiMini {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello team");
         System.out.println("kk");
-=======
-        System.out.println("ok");
->>>>>>> 341f7daec59d3a14a122235065e7e1b4c2b24f0b
     }
 }
