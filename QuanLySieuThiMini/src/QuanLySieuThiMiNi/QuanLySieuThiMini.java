b695eea6c9428fc59010cc92c0f8aff188c6f1b0
LaperCode <lapper2005@gmail.com>
2024-11-05 13:45:48 +0700

    1

--- QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
+++ QuanLySieuThiMini/src/QuanLySieuThiMiNi/QuanLySieuThiMini.java
@@ -3,6 +3,6 @@ package QuanLySieuThiMiNi;
 public class QuanLySieuThiMini {
     public static void main(String[] args) {
         System.out.println("Hello team");
-
+        System.out.println("kk");
     }
 }
